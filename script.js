7ab0797bcef6d8d0bdb67fcb50b5aba84d51af36
SamanthaNguyen1 <>
2021-05-13 01:03:19 +0000

    Play again button

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 // connects the "Guess" the button to the "random" function
 document.getElementById('button').addEventListener('click', randomizer)
 
-document.getElementById('play-again').addEventListener('click', randomizer)
+document.getElementById('play-again').addEventListener('click', playagain)
 
 let randomNumber = 0
 let userGuess = 0
+
+  // generates a random number between 1 to 6
+  randomNumber = Math.floor(Math.random() * 6) + 1
 
 function randomizer () {
   // gets the number typed in by the user and convert into an integer
   userGuess = document.getElementById('input').value
   userGuess = parseInt(userGuess)
 
-  // generates a random number between 1 to 6
-  randomNumber = Math.floor(Math.random() * 6) + 1
-
   // compares userGuess and randomNumber
-  if (userGuess == randomNumber) {
+  if (userGuess === randomNumber) {
     document.getElementById('answer').innerHTML = 'Congrats, you guessed correctly!'
   } else if (userGuess >= randomNumber) {
-    document.getElementById('answer').innerHTML = 'Try lower'
+    document.getElementById('answer').innerHTML = 'Try a lower number'
   } else {
-    document.getElementById('answer').innerHTML = 'Try higher'
+    document.getElementById('answer').innerHTML = 'Try a higher number'
   }
+}
+
+
+function playagain () {
+  randomNumber =  Math.floor(Math.random() * 6) + 1
+  document.getElementById('answer').innerHTML = ''
 }
